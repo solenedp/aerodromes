808f8392b81e6f4b6d8b9bf2b453189b23c8648d
pa.leveilley <pa.leveilley@groupeonepoint.com>
2022-10-03 13:05:21 +0200

    second commit

--- aeorodromes.jl
+++ aeorodromes.jl
@@ -1,6 +1,8 @@
-using JuMp, Gurobi
+using JuMP, Gurobi
 
-model = Model(Gurobi.Optimizer)
+include("read.jl")
+readInstance("instances aerodromes-20221003/aerodrome_20_1.txt")
+# model = Model(Gurobi.Optimizer)
 #déclaration des variables
 
 
